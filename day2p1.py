d4fe33646c91737d334610bf498274e49f66ee2d
Anarkali96 <123364572+Anarkali96@users.noreply.github.com>
2023-03-31 22:39:17 +0530

    Add files via upload
    Today I used for and if else statements . For statement is easy to use but if else I find it difficult.But they are basic hence one has to do mastery on them.

---
+++ day2p1.py
@@ -0,0 +1,6 @@
+import math as m
+z=500
+n=m.ceil(m.sqrt(z))
+for i in range(1,n,1) :
+    a=i*i
+    print(a)
---
+++ day2p2.py
@@ -0,0 +1,7 @@
+# To Print number is odd or even
+n=int(input('Enter the number'))
+d=n%2
+if d==0:
+    print('Number is even')
+else :
+    print('Number is odd')
