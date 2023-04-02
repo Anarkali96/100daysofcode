f58a7c96da416539069dd718d8c7cd2184c7a7b9
Anarkali96 <123364572+Anarkali96@users.noreply.github.com>
2023-04-02 14:45:05 +0530

    Add files via upload

---
+++ Day4p1.py
@@ -0,0 +1,13 @@
+i=1
+j=1
+a = 'Anarkali'
+# print(a)
+z=a
+while i<=5:
+
+    while j<=5:
+        b= ' Awesumely beautiful'
+        z=z+b
+        j=j+1
+    print(z)
+    i=i+1
---
+++ Day4p2.py
@@ -0,0 +1,13 @@
+a=int(input('Enter first no.'))
+b=int(input('Enter 2nd no.'))
+c=int(input('Enter 3rd no.'))
+if a>b:
+    if a>c:
+        print('a is greatest')
+elif b>c:
+    print('b is greatest')
+elif c>a:
+    print('c is greatest')
+
+
+
